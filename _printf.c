7a8a1497fa8832ec2d3e31c3b61906872e21f35d
Uzo Pius Enyum <uzopiusenyum@gmail.com>
2023-03-28 10:08:09 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -10,11 +10,8 @@ void print_buffer(char buffer[], int *buff_ind);
 int _printf(const char *format, ...)
 
 {
-
 int i, printed = 0, printed_chars = 0;
-
 int flags, width, precision, size, buff_ind = 0;
-
 va_list list;
 char buffer[BUFF_SIZE];
 if (format == NULL)
@@ -43,6 +40,7 @@ flags, width, precision, size);
 if (printed == -1)
 return (-1);
 printed_chars += printed;
+
 }
 }
 print_buffer(buffer, &buff_ind);
@@ -60,9 +58,7 @@ return (printed_chars);
 void print_buffer(char buffer[], int *buff_ind)
 
 {
-
 if (*buff_ind > 0)
 write(1, &buffer[0], *buff_ind);
 *buff_ind = 0;
-
 }
